c248c0bd45eafd9e66e9533a2f42db96ef1ad9a4
Prajapati__Hardik__24 <hardikprajapati242008@gmail.com>
2026-06-24 21:11:08 +0530

    Time: 3 ms (39.97%), Space: 8.9 MB (25.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,22 @@
+bool isPalindrome(int x) {
+    int rem = 0 ,tmp = x;
+    long rev_num = 0 ;
+    if(x < 0 || (x % 10 == 0 && x != 0))
+    {
+        return false;
+    }
+    while(tmp != 0)
+    {
+        rem = tmp % 10;
+        rev_num = (rev_num*10)+rem;
+        tmp = tmp / 10;
+    }
+    if(rev_num == x)
+    {
+       return true;
+    }
+    else
+    {
+        return false;
+    }
+}
