363686b5afc0904b89654a9302ffd62fd1745578
Prajapati__Hardik__24 <hardikprajapati242008@gmail.com>
2026-07-03 10:22:29 +0530

    Time: 0 ms (100%), Space: 42.8 MB (14.79%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if( (n > 0) && ( (n-1) % 3 == 0) )
+        {
+            if ( (n > 0) && (n & (n - 1)) ==  0  )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
