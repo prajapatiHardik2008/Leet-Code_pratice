ca432e6d6ffae275f49505a48be2ee0889cb96f6
Prajapati__Hardik__24 <hardikprajapati242008@gmail.com>
2026-07-06 17:34:59 +0530

    Time: 1 ms (96.04%), Space: 42.8 MB (89.43%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder res = new StringBuilder();
+        int i;
+        
+        for(i = 0 ; i < Math.min(word1.length() , word2.length() ) ; i++)
+        {
+            res.append(word1.charAt(i));
+            res.append(word2.charAt(i));
+        }
+        if( i < word1.length())
+        {
+            res.append(word1.substring(i,word1.length()));
+        }
+        else if (i <  word2.length())
+        {
+            res.append(word2.substring(i,word2.length()));
+        }
+        return res.toString();
+    }
+}
