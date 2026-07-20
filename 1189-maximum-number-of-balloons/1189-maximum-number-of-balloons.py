a22a27bc18f5a63e62a6dfcf7e4a13ebdeedaba8
Prajapati__Hardik__24 <hardikprajapati242008@gmail.com>
2026-07-20 20:15:31 +0530

    Time: 11 ms (19.37%), Space: 12.5 MB (47.07%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -13,15 +13,10 @@ class Solution(object):
         }
         for Ch in text:
             if Ch == 'b' or Ch == 'a' or Ch == 'l' or Ch == 'o' or Ch == 'n':
-                val = charFrq[Ch]+ 1
-                charFrq.update({Ch:val})
-        if charFrq['l'] < 2 or charFrq['o'] <2:
-            return 0
-        else:
-            val = int(charFrq['l']/2)
-            charFrq.update({'l':val})
-            val = int(charFrq['o']/2)
-            charFrq.update({'o':val})
+                charFrq[Ch] += 1
+    
+        charFrq['l'] //= 2
+        charFrq['o'] //= 2
         return min(charFrq.values())
 
         
