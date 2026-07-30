9edc7370db9fd15bec08d66930396291baeaa603
Prajapati__Hardik__24 <hardikprajapati242008@gmail.com>
2026-07-30 21:09:07 +0530

    Time: 0 ms (100%), Space: 43.3 MB (28.23%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,23 +1,10 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] freq = new int[26];
+        int n = word.length();
+        int rem = n % 8;
+        int comp = n / 8;
 
-        for (char ch : word.toCharArray()) {
-            freq[ch - 'a']++;
-        }
-
-        Arrays.sort(freq);
-
-        int ans = 0;
-        int index = 0;
-
-        for (int i = 25; i >= 0; i--) {
-            if (freq[i] == 0) break;
-
-            int cost = (index / 8) + 1;
-            ans += freq[i] * cost;
-            index++;
-        }
+        int ans = 8 * comp * (comp + 1) / 2 + (comp + 1) * rem;
 
         return ans;
     }
