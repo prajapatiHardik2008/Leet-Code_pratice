8f8c4ef62ca48af5e777666246bb0ca1c94506dd
Prajapati__Hardik__24 <hardikprajapati242008@gmail.com>
2026-07-31 23:34:45 +0530

    Time: 1 ms (78.38%), Space: 42.3 MB (39.83%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isThree(int n) {
+        int c = 2 ;
+        for(int i = 2 ; i <= n/2; i++ )
+        {
+            if( n % i == 0)
+            {
+               c++;
+            }
+        }
+        return c==3;
+    }
+}
