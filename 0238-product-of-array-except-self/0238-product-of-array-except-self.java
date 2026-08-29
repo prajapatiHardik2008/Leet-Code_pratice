63817bb4a82f2ee05006925dd2a900bb9477304b
Hardik__24 <hardikprajapati242008@gmail.com>
2026-08-29 22:09:52 +0530

    Time: 2 ms (93.45%), Space: 72.2 MB (16.72%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = 1;
+        
+        // it will give us to left prefix ans = [1 , 1, 2, 6]
+        for(int i = 1; i < nums.length; i++)
+        {
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+        // now we'll mult. the right subf. to the array ans [ 24, 12, 8 , 6]
+        int right  = 1;
+        for(int i = nums.length - 1 ; i >= 0 ; i--)
+        {
+            ans[i] = ans[i] * right;
+            right = right * nums[i];
+        }
+        
+        return ans;
+    }
+}
